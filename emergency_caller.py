91aa92527388a81bf4c4342f6dc8ba6df17b4a64
Tugrul Antay <tugruldemir2020@gmail.com>
2023-06-27 22:10:30 +0300

    a

---
+++ emergency_caller.py
@@ -0,0 +1,14 @@
+
+
+print("Are you phisically in danger?")
+
+answer = input("please quickly yes or no: ")
+
+if answer == "yes":
+    x = input ("Call police or ambulance?  :")
+    if x == "police" or x == "ambulance":
+        b = input ("please quick put your name :")
+        print("Calling them right now, STAY TIGHT!..... ")
+
+else:
+    print("Please call back when you need us, bye bye now...")
