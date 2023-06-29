7a73c1f5284c73d299e13577416b38715259907e
Tugrul Antay <tugruldemir2020@gmail.com>
2023-06-29 11:35:38 +0300

    a

---
+++ guess_number_game.py
@@ -0,0 +1,21 @@
+import rondom
+
+numberlist = [
+"NUMBER 1#",
+"NUMBER 2#",
+"NUMBER 3#",
+"NUMBER 4#",
+"NUMBER 5#",
+"NUMBER 6#",
+"NUMBER 7#",
+"NUMBER 8#",
+"NUMBER 9#",
+"NUMBER 10#",
+]
+
+print("Think of a rondom number between 1 to 10!!!")
+answer = input("When you do, to let me know please type YES! :")
+
+if answer == "yes":
+    x = rondom.randrange(1,10)
+    print(numberlist[x])
