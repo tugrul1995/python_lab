6f9b041087d07c67343bf32f7737d3523c91757b
Tugrul Antay <tugruldemir2020@gmail.com>
2023-06-30 13:07:06 +0300

    a

---
+++ calculator.py
@@ -0,0 +1,19 @@
+import math
+
+#this functions are for the math modules in the project tool.
+
+#THIS ADD FUNCTION
+def add(x,y):
+    return x + y
+
+#THIS SUBTRACT FUNCTION
+def subtract(x,y):
+    return x - y
+
+#THIS DIVIDE FUNCTION
+def divide(x,y):
+    return x / y
+
+#THIIS MULTIPLY FUNCTION
+def multiply(x,y):
+    return x * y
