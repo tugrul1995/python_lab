2c8875b42cc33fcf9268a2d768eb4cbea092dab8
Tugrul Antay <tugruldemir2020@gmail.com>
2023-07-01 18:22:54 +0300

    a

---
+++ quiz_game.py
@@ -0,0 +1,12 @@
+#  QUESTION:
+
+print("Welcome to TJ best quiz game! Please answer each QUESTION: ")
+
+#QUESTIONS
+print("Question 1. What city is the capital of Trukey? :")
+userInput = input()
+
+if answer == 'ankara':
+    print("correct!")
+else:
+    print("sorry, answer is incorrect")
