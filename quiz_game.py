855f6dea10bc3c47ccbae06c696a6516e3f58d56
Tugrul Antay <tugruldemir2020@gmail.com>
2023-07-01 19:06:20 +0300

    a

--- quiz_game.py
+++ quiz_game.py
@@ -4,9 +4,26 @@ print("Welcome to TJ best quiz game! Please answer each QUESTION: ")
 
 #QUESTIONS
 print("Question 1. What city is the capital of Trukey? :")
-userInput = input()
+answer1 = input()
 
-if answer == 'ankara':
+if answer1 == 'ankara':
     print("correct!")
 else:
     print("sorry, answer is incorrect")
+
+print("Question 2. What city is the capital of Japan? :")
+answer2 = input()
+
+if answer2 == 'tokyo':
+    print("correct")
+else:
+    print("Go back to school baby!!")
+
+print("Question3. Does Demir's feet smell or not?")
+print("yes or no :")
+answer3 = input()
+
+if answer3 == 'yes':
+    print("correct, god damn it!")
+else:
+    print("POOR Judgement, answer better DAWG!!")
